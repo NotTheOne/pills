c51623e04ea874bd4a1412b498c135f836577691
Roger Camps <rogercampsny@gmail.com>
2013-09-15 17:17:34 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pills::Application.config.secret_token = 'f42219470bab0ebe1e2901a3d1786e3c2a4b35ab0a996076488942df3fcacaf714ee627de98f5af715ebd1ec5312118e16794d95ace66fcb5917f80d27c52c11'
