1018267df7a9d96317d38d034efe4144f91255eb
Roger Camps <rogercampsny@gmail.com>
2013-10-07 15:49:41 -0400

    home page

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe 'User enters home page' do
-	it 'welcomes user to the page' do
-		visit rooth_path
-		expect.page.to have_content('Welcome to Pills')
-		expect.page.to have_content('Log in')
-		expect.page.to have_content('Sign up')
+	it 'welcomes user to the home page' do
+		visit root_path
+		expect(page).to have_content('Pills App')
+		expect(page).to have_content('Log in')
+		expect(page).to have_content('Sign up')
 	end
 end
